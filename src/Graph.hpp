6e86d216a9ec482c57bcfebc5015a72b847d5f66
Luis Marques <ldx.marques@gmail.com>
2021-10-22 14:23:23 +0200

    syntax fix

--- src/Graph.hpp
+++ src/Graph.hpp
@@ -11,10 +11,11 @@ public:
 
     ~Graph() = default;
 
-    inline const std::vector<int>& getNeighbors(int vertex) { return mAdjacencyList[vertex]; }
+    inline const std::vector<int>& getNeighbors(int vertex) const { return mAdjacencyList[vertex]; }
 
     inline int getColor(int vertex) { return mVerticesColors[vertex]; }
     inline int getNbVertices() const {return static_cast<int>(mAdjacencyList.size());}
+
     inline int getDistance(int vertex1, int vertex2) const { return mDistances[vertex1][vertex2]; }
 
     friend std::ostream& operator<<(std::ostream& out, const Graph& graph);
